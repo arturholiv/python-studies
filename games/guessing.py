10b9879fdc94d8947d24a44cd3202c42e4bb16f4
Artur Henrique de Oliveira <artur.henrique45@hotmail.com>
2022-09-17 19:11:52 -0300

    feat: 🎸 implements points system

--- games/guessing.py
+++ games/guessing.py
@@ -4,9 +4,9 @@ print("*****************************")
 print("Welcome to the guessing game!")
 print("*****************************")
 
-secret_number = int(random.randrange(1, 25))
+secret_number = int(random.randrange(1, 100))
 total_attempts = 0
-
+points = 100
 print("Enter the difficulty level")
 print("(1) easy - (2) medium - (3) hard")
 
@@ -25,8 +25,8 @@ for round_is in range(1, total_attempts + 1):
     answer_int = int(answer_str)
     print("You typed: ", answer_str)
 
-    if answer_int < 1 or answer_int > 25:
-        print("You must type a number between 1 and 25")
+    if answer_int < 1 or answer_int > 100:
+        print("You must type a number between 1 and 100")
         print("Attempt {} of {}".format(round_is, total_attempts))
         continue
 
@@ -36,13 +36,13 @@ for round_is in range(1, total_attempts + 1):
 
     print("Attempt {} of {}".format(round_is, total_attempts))
     if got_it_right:
-        print("You're right")
+        print("You're right and scored {} points".format(points))
         break
     else:
         if higher:
             print("You're wrong! :/ Your attempt was higher than the number ")
         elif lowest:
             print("You're wrong! :/ Your attempt was lower than the number ")
-            if answer_int == 3:
-                print("The right answer was {}".format(secret_number))
-print("Game fished")
+        points_lost = abs(secret_number - answer_int)
+        points = points - points_lost
+print("Game fished! Total points: {}".format(points))
