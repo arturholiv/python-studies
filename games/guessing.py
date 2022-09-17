ff79021cd3ff262db1b971cb133b9e3a06503e32
Artur Henrique de Oliveira <artur.henrique45@hotmail.com>
2022-09-17 13:56:33 -0300

    refactor: 💡 refactoring

--- games/guessing.py
+++ games/guessing.py
@@ -7,10 +7,15 @@ total_attempts = 3
 
 
 for round_is in range(1, total_attempts + 1):
-    answer_str = input("Type your number: ")
+    answer_str = input("Type a number between 1 and 15: ")
 
     answer_int = int(answer_str)
     print("You typed: ", answer_str)
+
+    if answer_int < 1 or answer_int > 15:
+        print("You must type a number between 1 and 15")
+        print("Attempt {} of {}".format(round_is, total_attempts))
+        continue
 
     got_it_right = answer_int == secret_number
     higher = answer_int > secret_number
@@ -19,9 +24,10 @@ for round_is in range(1, total_attempts + 1):
     print("Attempt {} of {}".format(round_is, total_attempts))
     if got_it_right:
         print("You're right")
-        round_is = total_attempts
+        break
     else:
         if higher:
             print("You're wrong! :/ Your attempt was higher than the number ")
         elif lowest:
             print("You're wrong! :/ Your attempt was lower than the number ")
+print("game over")
