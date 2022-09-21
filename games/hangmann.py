8a39dfabc2841e1b6c7b3373ce10addf1a352fd2
Artur Henrique de Oliveira <artur.henrique45@hotmail.com>
2022-09-21 17:17:28 -0300

    feat: 🎸 firs implementation of the function

--- games/hangmann.py
+++ games/hangmann.py
@@ -1,9 +1,25 @@
 def play():
     print("*****************************")
-    print("Welcome to the hangman game!")
+    print("*Bem vindo ao jogo da Forca!*")
     print("*****************************")
 
-    print("Game fished!")
+    palavra_sercreta = "carro"
+
+    enforcou = False
+    acertou = False
+
+    while not enforcou and not acertou:
+        chute = input("Digite uma letra:  ").lower()
+
+        index = 1
+        for letra in palavra_sercreta:
+            if chute == letra:
+                print("Letra {} correta na posição {}".format(letra, index))
+            index = index + 1
+        # print("jogando...")
+
+    print("Fim do jogo!")
+
 
 
 if __name__ == "__main__":
